a78f5878c68e562f4e7f9a817848f1582a982707
刘桐彤 <liutongtong@ME-MacBook-Pro.lan>
2014-11-13 01:16:40 +0800

    information site domain modified

--- queryhandler/settings.py
+++ queryhandler/settings.py
@@ -5,4 +5,4 @@ QRCODE_URL = 'http://tsinghuaqr.duapp.com/'
 SITE_DOMAIN = 'http://wx2.igeek.asia'
 SITE_HTTP_PROTOCOL = 'http'
 
-INFORMATION_SITE_DOMAIN = 'http://wx2.igeek.asia'
+INFORMATION_SITE_DOMAIN = 'http://tuantuan.ssast.org'
