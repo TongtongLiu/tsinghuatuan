27365b30cd5666c605aba476ebdaff5f01917adf
stefaniexin <stefanie_xin@163.com>
2014-11-19 21:52:52 +0800

    no message

--- urlhandler/adminpage/static/js/activity_detail.js
+++ urlhandler/adminpage/static/js/activity_detail.js
@@ -478,52 +478,6 @@ function publishActivity() {
     return false;
 }
 
-var isSelected = false;
-function bind_click(){
-    var valid_list = document.getElementsByClassName('valid');
-    var inputSelect = document.getElementById('input');
-    for (var i = 0; i < valid_list.length; i++) {
-        valid_list[i].onclick = function (evt) {
-            if (this.getAttribute('class') == 'valid') {
-                if(isSelected == false) {
-                    this.setAttribute('class', 'selectThis');
-                    inputSelect.value = this.getAttribute('id');
-                    showText(this.getAttribute('id'));
-                    isSelected = true;
-                } else {
-                    document.getElementsByClassName('selectThis')[1].setAttribute('class', 'valid');
-                    this.setAttribute('class', 'selectThis');
-                    inputSelect.value = this.getAttribute('id');
-                    showText(this.getAttribute('id'));
-                }
-            } else if ((this.getAttribute('class') == 'selectThis')) {
-                this.setAttribute('class', 'valid');
-                hideText();
-                inputSelect.value = '';
-                isSelected = false;
-            }
-
-            function showText(seat){
-                var location = seat.split('-');
-                document.getElementById('row').innerText = location[0];
-                document.getElementById('column').innerText = location[1];
-                document.getElementById('showSelection').style.display = 'block';
-                document.getElementById('submitButton').disabled = false;
-                document.getElementById('alert').style.display = 'none';
-            }
-
-            function hideText(){
-                document.getElementById('showSelection').style.display = 'none';
-                document.getElementById('alert').innerText = '请选择您的座位';
-                document.getElementById('alert').style.display = 'block';
-                document.getElementById('submitButton').disabled = true;
-            }
-        }
-    }
-}
-
-bind_click();
-
 
 initializeForm(activity);
 showForm();
