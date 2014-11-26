cd87a66a637c52789e95909cc2672afe5e127fab
liutongtong <jshaltt7@163.com>
2014-11-26 12:38:54 +0800

    token

--- urlhandler/userpage/views.py
+++ urlhandler/userpage/views.py
@@ -386,8 +386,7 @@ def uc_token(request, openid):
         weixin_id = request.POST.get('openid', '')
         user = User.objects.filter(weixin_id=weixin_id, status=1)
         timestamp = int(time.time()) / 100
-        # rtnJSON = {'token': user[0].stu_id ^ timestamp}
-        rtnJSON = {'token': '123'}
+        rtnJSON = {'token': user[0].stu_id ^ timestamp}
         return HttpResponse(json.dumps(rtnJSON), content_type='application/json')
     else:
         if User.objects.filter(weixin_id=openid, status=1).exists():
