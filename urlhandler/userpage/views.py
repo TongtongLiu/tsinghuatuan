e3e3a97c4668174a854f3f9b4fe92213a6fedaff
wegroup <jshaltt7@163.com>
2014-11-27 03:16:16 +0000

    2ticket

--- urlhandler/userpage/views.py
+++ urlhandler/userpage/views.py
@@ -368,15 +368,20 @@ def uc_account(request, openid):
         if request.method == 'POST':
             try:
                 user.update(status=0)
+                print '0/r/n'
                 binds1 = Bind.objects.filter(active_stu_id=user[0].stu_id)
+                print '1/r/n'
                 for bind in binds1:
+                    print '2/r/n'
                     user.update(bind_count=F('bind_count')-1)
-                    User.objects.filter(passive_stu_id=bind.passive_stu_id).update(bind_count=F('bind_count')-1)
+                    User.objects.filter(stu_id=bind.passive_stu_id).update(bind_count=F('bind_count')-1)
+                print '3/r/n'
                 binds1.delete()
+                print '4/r/n'
                 binds2 = Bind.objects.filter(passive_stu_id=user[0].stu_id)
                 for bind in binds2:
                     user.update(bind_count=F('bind_count')-1)
-                    User.objects.filter(passive_stu_id=bind.active_stu_id).update(bind_count=F('bind_count')-1)
+                    User.objects.filter(stu_id=bind.active_stu_id).update(bind_count=F('bind_count')-1)
                 binds2.delete()
             except:
                 return HttpResponse('logout error')
