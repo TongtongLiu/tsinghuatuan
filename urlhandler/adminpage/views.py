16850f6b0dbbcf6f53ee83229229cfa0017cc468
stefaniexin <stefanie_xin@163.com>
2014-11-30 03:04:10 +0800

    admin

--- urlhandler/adminpage/views.py
+++ urlhandler/adminpage/views.py
@@ -257,7 +257,7 @@ def wrap_activity_dict(activity):
 def wrap_seat(seats):
     rt = []
     for x in seats:
-        temp = str(x.position_row) + '-' + str(x.position_column)
+        temp = str(x.position_row).encode('utf8') + '-' + str(x.position_column).encode('utf8')
         rt.append(temp)
     return rt
 
