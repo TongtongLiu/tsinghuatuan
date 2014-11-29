c5b6cf49d0e762aec884cb19a105c32e9c60b7da
liutongtong <jshaltt7@163.com>
2014-11-29 22:53:15 +0800

    prepare for test

--- tt_prepare_test.py
+++ tt_prepare_test.py
@@ -6,7 +6,7 @@ def create_test_users(num):
     for i in range(1, num+1, 1):
         try:
             openid = str(i).rjust(4, '0')
-            newuser = User.objects.create(weixin_id=openid, stu_id=str(-i), stu_name=str(i), stu_type="本科生", status=1)
+            newuser = User.objects.create(weixin_id=openid, stu_id=str(-i), stu_name=str(i), stu_type=u"本科生", status=1)
             newuser.save()
         except:
             break
