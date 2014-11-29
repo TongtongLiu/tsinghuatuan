deb7daabbbbc2faff689c7eedcccd1d73e9dbe14
liutongtong <jshaltt7@163.com>
2014-11-29 23:18:07 +0800

    prepare for test

--- urlhandler/userpage/tt_prepare_test.py
+++ urlhandler/userpage/tt_prepare_test.py
@@ -1,8 +1,24 @@
 #-*- coding:utf-8 -*-
 
-__author__ = 'liutongtong'
-
-from urlhandler.models import User
+from django.db.models import F, Q
+from django.http import HttpResponse, Http404
+from django.template import RequestContext
+from django.shortcuts import render_to_response
+from urlhandler.models import User, Activity, Ticket, Bind
+from django.views.decorators.csrf import csrf_exempt
+from urlhandler.settings import STATIC_URL
+import urllib, urllib2
+import datetime, time
+import json
+from django.db import transaction
+from django.utils import timezone
+from weixinlib.weixin_urls import WEIXIN_URLS
+from weixinlib import http_get
+from django.shortcuts import redirect
+from userpage.safe_reverse import *
+import string
+import random
+from weixinlib.settings import WEIXIN_APPID
 
 def create_test_users(num):
     for i in range(1, num+1, 1):
