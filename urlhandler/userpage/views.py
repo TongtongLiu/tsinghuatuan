1d3f673811dd3088249f9e40732bdb51a2b3e29a
thsslimeng <thusw21lm@126.com>
2014-12-11 00:34:55 +0800

    样式

--- urlhandler/userpage/views.py
+++ urlhandler/userpage/views.py
@@ -249,8 +249,8 @@ def details_view(request, activityid):
     act_photo = activity[0].pic_url
     cur_time = timezone.now()  # use the setting UTC
     act_seconds = 0
-    if act_book_start <= cur_time <= act_bookend:
-        act_delta = act_bookend - cur_time
+    if act_book_start <= cur_time <= act_book_end:
+        act_delta = act_book_end - cur_time
         act_seconds = act_delta.total_seconds()
         act_status = 0  # during book time
     elif cur_time < act_book_start:
@@ -260,7 +260,7 @@ def details_view(request, activityid):
     else:
         act_status = 2  # after book time
     variables = RequestContext(request, {'act_name': act_name, ' act_text': act_text, 'act_photo': act_photo,
-                                      'act_book_start': act_book_start, 'act_bookend': act_book_end, ' act_begintime': act_begin_time,
+                                      'act_book_start': act_book_start, 'act_book_end': act_book_end, ' act_begin_time': act_begin_time,
                                       'act_end_time': act_end_time, 'act_totaltickets': act_total_tickets, 'act_key': act_key,
                                       'act_place': act_place, 'act_status': act_status, 'act_seconds': act_seconds, 'cur_time':cur_time,
                                       'act_abstract': act_abstract, 'act_text_status': act_text_status, 'act_ticket_remain': act_ticket_remain})
