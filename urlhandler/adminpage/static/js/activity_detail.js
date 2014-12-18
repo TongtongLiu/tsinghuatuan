237b5e78ae984477bf2fe1cf79cf63e6d8e8a3bf
stefaniexin <xy-zhao12@mails.tsinghua.edu.cn>
2014-12-18 22:33:42 +0800

    adminpage

--- urlhandler/adminpage/static/js/activity_detail.js
+++ urlhandler/adminpage/static/js/activity_detail.js
@@ -514,7 +514,7 @@ function calculateTickets(){
 function recordTicketsCount(){
     var count = $('#count').text();
     $('#input-total_tickets').val(count);
-    //$('#input-total_tickets').attr("disabled","disabled");
+    $('#input-total_tickets').attr("disabled","readOnly");
 }
 
 function clearSeat() {
@@ -550,13 +550,12 @@ function changePlace() {
     if(a.val() == "1"){//综体
         b.attr("data-target","#modal_zongti");
         $('#chooseSeat').removeAttr("disabled");
-        //$('#input-total_tickets').attr("disabled","disabled");
+        $('#input-total_tickets').attr("disabled","readOnly");
     }
     else if(a.val() == "2"){//新清华学堂
         b.attr("data-target","#modal_xinqing");
         $('#chooseSeat').removeAttr("disabled");
-        //$('#input-total-tickets').css("display","none");
-        //$('#input-total_tickets').attr("disabled","disabled");
+        $('#input-total_tickets').attr("disabled","readOnly");
     }
     else if(a.val() == "0"){
         b.attr("data-target","#modal_none");
